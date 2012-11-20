23d635e07b91d99602ff524ef0a240aafa692cdb
Martin Kralik <majak47@gmail.com>
2012-11-20 16:23:35 +0100

    Pridana podpora pre obrazky.
    Obrazok ma v schedule.txt navyse jedno cislo,
    ktore urcuje ako dlho sa ma prehravat.
    Podla neho sa vytvori video, ktore sa bude prehravat.

--- infoboard/video.py
+++ infoboard/video.py
@@ -1,25 +1,38 @@
 import os.path
 import logging
+import subprocess
 from datetime import date
+from subprocess import PIPE
 
 class Video:
     """Parses line containing description of a video.
 
     Line must contain filename followed by duration (two dates):
     test.avi 2012-01-12 2012-02-07
+    In case of picture additional number (duration in seconds) is required:
+    notice.jpg 2013-01-01 2014-12-31 30
 
-    This video would be played from 12th january 2012 to 7th february 2012.
-
-    ValueError is thrown in case that specified file does not exists
-    in 'videos' subfolder or there are not enough arguments.
+    ValueError is thrown in case that specified video does not exists
+    in 'videos' subfolder, picture is not in 'pictures' subfolder
+    or there are not enough arguments.
     Possible reason is logged in 'infoboard.video' logger.
     """
     def __init__(self, line):
         try:
-            filename, first_day, last_day = line.split(' ')
-            filename = 'videos/' + filename
-            if not os.path.isfile(filename):
-                raise ValueError('Video "{0}" does not exist'.format(filename))
+            args = line.split(' ')
+            filename, first_day, last_day = args[:3]
+            if len(args) == 4:
+                duration = args[3]
+                self._from_image(filename, duration)
+                if not os.path.isfile('pictures/' + filename):
+                    raise ValueError('Picture "{0}" does not exist'
+                        .format(filename))
+                filename = 'videos/' + filename + '.' + duration + '.avi'
+            else:
+                filename = 'videos/' + filename
+                if not os.path.isfile(filename):
+                    raise ValueError('Video "{0}" does not exist'
+                        .format(filename))
             self.filename = filename
             self.start_date = date(*map(int, first_day.split('-')))
             self.end_date = date(*map(int, last_day.split('-')))
@@ -33,3 +46,22 @@ class Video:
             logging.getLogger('infoboard.video').error(message)
             raise e
 
+    def _from_image(self, filename, duration):
+        """Created video from picture with requested duration
+        unless it already exists."""
+        if not os.path.isfile('videos/' + filename + '.' + duration +  '.avi'):
+            command = ['/usr/bin/mencoder',
+                       # reads commands from stdin
+                       'mf://pictures/' + filename,
+                       # don't quit when there is no file to play
+                       '-o', 'videos/' + filename + '.' + duration + '.avi',
+                       # fullscreen
+                       '-ovc', 'lavc',
+                       # no way to control mplayer in other way
+                       '-lavcopts', 'vcodec=mjpeg', 
+                       # reuse same window for playing all videos
+                       '-fps', '1/' + duration,
+                       # don't print progress
+                       '-ofps', '30'
+                      ]
+            self.mp_process = subprocess.Popen(command)
